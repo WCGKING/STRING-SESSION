e571e6375c786e3c5b2f520349faff821fe8588e
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-09 15:43:06 +0530

    STRING-SESSION

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
-
+#OWNER_ID = int(getenv("6752444356"))
+OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 MUST_JOIN = getenv("MUST_JOIN", None)
