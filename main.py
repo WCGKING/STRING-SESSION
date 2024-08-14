414bb9a997dcf32e5583b217753773de6bcde7b0
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-08-14 07:36:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ logging.basicConfig(
     level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
-StartTime = time.time()
+logging.getLogger("pymongo").setLevel(logging.ERROR)
+
+# Initialize the Client
 app = Client(
-    "Anonymous",
+    "BRANDED",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,
     in_memory=True,
-    plugins=dict(root="StringGenBot"),
+    plugins=dict(root="SESSIONGEN"),
 )
 
 
